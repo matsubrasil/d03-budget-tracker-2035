fdbc791f813a5bc31b354d81c183879c50104b4d
matsubrasil <matsuboy@outlook.com>
2025-05-28 14:12:09 -0300

    (feat) add wizard page & cambobox component & tanstack query

--- src/components/CurrencyComboBox.tsx
+++ src/components/CurrencyComboBox.tsx
@@ -24,41 +24,113 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 import { Currencies, Currency } from '@/lib/currencies'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import SkeletonWrapper from '@/components/SkeletonWrapper'
+import { UserSettings } from '@/generated/prisma'
+import { UpdateUserCurrency } from '@/app/wizard/_actions/userSettings'
+import { toast } from 'sonner'
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false)
   const isDesktop = useMediaQuery('(min-width: 768px)')
-  const [selectOption, setSelectOption] = React.useState<Currency | null>(null)
+  const [selectedOption, setSelectedOption] = React.useState<Currency | null>(
+    null
+  )
+
+  const userSettings = useQuery<UserSettings>({
+    queryKey: ['userSettings'],
+    queryFn: () => fetch('/api/user-settings').then((res) => res.json()),
+  })
+
+  React.useEffect(() => {
+    if (!userSettings.data) {
+      return
+    }
+
+    const userCurrency = Currencies.find(
+      (currency) => currency.value === userSettings.data.currency
+    )
+
+    if (userCurrency) {
+      setSelectedOption(userCurrency)
+    }
+  }, [userSettings.data])
+
+  // mutations are typically used to create/update/delete data or perform server side-effects
+  const mutation = useMutation({
+    mutationFn: UpdateUserCurrency,
+    onSuccess: (data: UserSettings) => {
+      toast.success(`Currency updated successfully 🍕`, {
+        id: 'update-currency',
+      })
+
+      setSelectedOption(
+        Currencies.find((c) => c.value === data.currency) || null
+      )
+    },
+    onError: (e) => {
+      toast.error('Something went wrong', {
+        id: 'update-currency',
+      })
+    },
+  })
+
+  const selectOptions = React.useCallback(
+    (currency: Currency | null) => {
+      if (!currency) {
+        toast.error('Please select a currency')
+        return
+      }
+      toast.loading('Updating currency...', {
+        id: 'update-currency',
+      })
+
+      mutation.mutate(currency.value)
+    },
+    [mutation]
+  )
 
   if (isDesktop) {
     return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
-            {selectOption ? <>{selectOption.label}</> : <>Set currency</>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectOption={setSelectOption} />
-        </PopoverContent>
-      </Popover>
+      <SkeletonWrapper isLoading={userSettings.isFetching}>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              className="w-full justify-start"
+              disabled={mutation.isPending}
+            >
+              {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0" align="start">
+            <OptionList setOpen={setOpen} setSelectOption={selectOptions} />
+          </PopoverContent>
+        </Popover>
+      </SkeletonWrapper>
     )
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-start">
-          {selectOption ? <>{selectOption.label}</> : <>Set currency</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerTitle className="hidden">Set currency</DrawerTitle>
-        <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectOption={setSelectOption} />
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button
+            variant="outline"
+            className="w-full justify-start"
+            disabled={mutation.isPending}
+          >
+            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerTitle className="hidden">Set currency</DrawerTitle>
+          <div className="mt-4 border-t">
+            <OptionList setOpen={setOpen} setSelectOption={selectOptions} />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </SkeletonWrapper>
   )
 }
 
--- src/components/providers/RootProviders.tsx
+++ src/components/providers/RootProviders.tsx
@@ -1,18 +1,25 @@
 'use client'
 
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function RootProviders({ children }: { children: ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemesProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
